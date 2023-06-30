b428856493681989f47bb47c1d1e73e7fb5a7589
BrunoSoffo <brussoffo@gmail.com>
2023-06-30 00:30:04 -0300

    script js

---
+++ static/Js/script.js
@@ -0,0 +1,14 @@
+let teacherForms = document.getElementsByClassName("teacher-contract")
+
+for (let teacherForm of teacherForms){
+    teacherForm.addEventListener("submit", function(event){
+        event.preventDefault 
+        // evita a atualização da pág após o submit
+
+        let price = teacherForm.price.value
+        let hours = teacherForm.hours.value
+        let total = price
+
+        alert(total)
+    })
+}
